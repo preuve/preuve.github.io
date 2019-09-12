71effd5047e78434c7980aa1583271e12dbc266c
preuve <joel.duet@free.fr>
2019-09-12 19:02:14 +0200

    gitignore

--- cours/derivation/bower_components/purescript-event/README.md
+++
@@ -1,5 +0,0 @@
-# purescript-event
-
-The `Event` type, extracted from my `purescript-behaviors` library.
-
-- [Module Documentation](generated-docs/FRP/)
--- cours/derivation/bower_components/purescript-web-events/src/Web/Internal/FFI.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-exports._unsafeReadProtoTagged = function (nothing, just, name, value) {
-  var ty = window[name];
-  if (ty != null && value instanceof ty) {
-    return just(value);
-  }
-  return nothing;
-};
