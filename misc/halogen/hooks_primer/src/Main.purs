0d66022169408393b9fcaecf070326195af0404f
preuve <joel.duet@free.fr>
2021-02-10 13:27:01 +0100

    hook #0

---
+++ misc/halogen/hooks_primer/src/Main.purs
@@ -0,0 +1,23 @@
+module Main where
+
+import Prelude
+
+import Data.Const (Const)
+import Effect (Effect)
+import Effect.Class (class MonadEffect)
+import Halogen as H
+import Halogen.Aff as HA
+import Halogen.Hooks as Hooks
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Halogen.VDom.Driver (runUI)
+import Halogen.HTML.Properties as HP
+
+page :: forall m. H.Component HH.HTML (Const Void) Unit Void m 
+page = Hooks.component \_ _ -> Hooks.do
+ Hooks.pure do
+    HH.p_ [ HH.text "Minimal component"]
+
+main :: Effect Unit
+main = HA.runHalogenAff $
+       HA.awaitBody >>= runUI page unit
