eb9349d71359fcfcb83634e536d640e5b2c253ef
preuve <joel.duet@free.fr>
2021-02-11 10:16:58 +0100

    hooks #2

--- misc/halogen/hooks_primer/src/Main.purs
+++ misc/halogen/hooks_primer/src/Main.purs
@@ -3,22 +3,26 @@ module Main where
 import Prelude
 
 import Data.Const (Const)
+import Data.Maybe (Maybe(..))
+import Data.Tuple.Nested ((/\))
 import Effect (Effect)
 import Effect.Class (class MonadEffect)
 import Halogen as H
 import Halogen.Aff as HA
-import Halogen.Hooks as Hooks
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
-import Halogen.VDom.Driver (runUI)
 import Halogen.HTML.Properties as HP
+import Halogen.Hooks as Hooks
+import Halogen.VDom.Driver (runUI)
 
---page :: forall m. H.Component HH.HTML (Const Void) Unit Void m 
+page :: forall m. H.Component HH.HTML (Const Void) Unit Void m 
 page = Hooks.component \_ _ -> Hooks.do
+  count /\ countId <- Hooks.useState 0
   Hooks.pure do
     HH.div_ 
       [ HH.label_ [ HH.text "minimal component"]
       , HH.p_ [ HH.text "can have multiple tags"]
+      , HH.button [HE.onClick $ const $ Just $ Hooks.modify_ countId (_ + 1)] [HH.text $ show count]
       ]
 
 main :: Effect Unit
